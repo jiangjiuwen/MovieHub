3891ec3691ba7f79147b7c6a0562ae8a1ff12425
jiangjiuwen <jiangjiuwen@outlook.com>
2018-04-17 21:53:50 +0800

    MovieHub.Core

---
+++ MovieHub.Core/Models/SubjectModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieHub.Core.Models
+{
+    public class SubjectModel
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string original_title { get; set; }
+        public Array aka { get; set; }
+        public string alt { get; set; }
+        public string mobile_url { get; set; }
+    }
+}
--- MovieHub.Core/Utilities/JsonHelper.cs
+++ MovieHub.Core/Utilities/JsonHelper.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MovieHub.Core
+namespace MovieHub.Core.Utilities
 {
-    public class Class1
+    class JsonHelper
     {
     }
 }
